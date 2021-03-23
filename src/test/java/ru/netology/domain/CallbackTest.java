543a29ba1fa3d5c46ed25e11b187183818da2f5b
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-03-23 11:02:31 +0300

    Add Test selenide

--- src/test/java/ru/netology/domain/CallbackTest.java
+++ src/test/java/ru/netology/domain/CallbackTest.java
@@ -1,27 +1,62 @@
 package ru.netology.domain;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CallbackTest {
     private WebDriver driver;
 
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Git\\chromedriver.exe");
+    }
+
     @BeforeEach
-    void  setUp() {
-      driver = new ChromeDriver();
+    void setUp() {
+        driver = new ChromeDriver();
     }
 
     @AfterEach
-    void  tearDown(){
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void  shouldTestSomething(){
-        throw new UnsupportedOperationException();
+    void shouldTestSomething() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий Пупкин");
+        elements.get(1).sendKeys("+79102280065");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    @Test
+    void shouldTestRequest() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Татьяна");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
 }
